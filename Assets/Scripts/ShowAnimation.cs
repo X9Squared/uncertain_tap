c43f242e682d925d99c2664d88809b4887034843
X9Squared <justin.wang91202@gmail.com>
2021-03-15 22:12:06 -0400

    functional showAnimation mk1

--- Assets/Scripts/ShowAnimation.cs
+++ Assets/Scripts/ShowAnimation.cs
@@ -1,18 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class ShowAnimation : MonoBehaviour
 {
+    //Setup variables for the rest of the program.
+    public Object[] animImport;
+    public List<VideoClip> anims = new List<VideoClip>();
+    public VideoPlayer viewer;
+
+    public GameObject attached;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Set up the videosource to be able to play the clips
+        viewer = attached.GetComponent<VideoPlayer>();
+        //Load all the clips from the Resources folder (talk to me if you want to know more
+        //about exactly what the Resources folder is)
+        animImport = Resources.LoadAll("VideoAssets", typeof(VideoClip));
+        //Load the objects - since Resources.LoadAll outputs only objects - into the List of
+        //audioClips (recasting the objects accordingly)
+        foreach (Object tmp in animImport)
+        {
+            VideoClip temp = (VideoClip)tmp;
+            anims.Add(temp);
+        }
+        viewer.source = VideoSource.VideoClip;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            viewer.clip = anims[0];
+            viewer.Prepare();
+            viewer.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            viewer.clip = anims[1];
+            viewer.Prepare();
+            viewer.Play();
+        }
     }
+
 }
