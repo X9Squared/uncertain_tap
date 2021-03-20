53922a8d2ee8e8eb0a1ba9faaa59680f3324ac7a
X9Squared <justin.wang91202@gmail.com>
2021-03-20 17:18:12 -0400

    Hue change implementation

---
+++ Assets/Scripts/HueChange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HueChange : MonoBehaviour
+{
+    public Image panel;
+    public GameObject attached;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel = attached.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            panel.color = newColor();
+        }
+    }
+
+    Color newColor()
+    {
+        return new Color(Random.value, Random.value, Random.value, 0.2f);
+    }
+}
